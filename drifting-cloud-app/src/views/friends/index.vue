8af5430e6e9bea62f96de2020fc51ed4ef00def0
enjsky.g <G506512418@163.com>
2021-05-04 18:19:38 +0800

    enjsky.g submited the project drifting-cloud-app on 2021.05.04

--- drifting-cloud-app/src/views/friends/index.vue
+++ drifting-cloud-app/src/views/friends/index.vue
@@ -47,30 +47,56 @@ time:2021-04-29
             <span class="pro_tongxunlu_span_bottom">看看右谁在抖音</span>
           </div>
           <!-- 查看按钮 -->
-          <div class="pro_tongxunlu_btn">查看</div>
+          <div class="pro_tongxunlu_btn">
+            <div class="tongxunlu_btn">查 看</div>
+          </div>
         </div>
         <!-- 功能：分享二维码、扫一扫、分享口令、面对面加朋友 -->
         <div class="pro_fun">
           <ul class="pro_fun_ul">
             <li class="pro_fun_li">
-              <i class="iconfont fun_li_left icon-erweima"></i>
-              <span class="fun_li_span">分享我的抖音二维码</span>
-              <i class="iconfont fun_li_right icon-youce"></i>
+              <div class="fun_li_i_left">
+                <i class="iconfont fun_li_left icon-erweima"></i>
+              </div>
+              <div class="fun_li_span">
+                <span>分享我的抖音二维码</span>
+              </div>
+              <div class="fun_li_i_right">
+                <i class="iconfont fun_li_right icon-youce"></i>
+              </div>
             </li>
             <li class="pro_fun_li">
-              <i class="iconfont fun_li_left icon-erweima"></i>
-              <span class="fun_li_span">分享我的抖音二维码</span>
-              <i class="iconfont fun_li_right icon-youce"></i>
+              <div class="fun_li_i_left">
+                <i class="iconfont fun_li_left icon-saoyisao"></i>
+              </div>
+              <div class="fun_li_span">
+                <span>扫一扫添加朋友</span>
+              </div>
+              <div class="fun_li_i_right">
+                <i class="iconfont fun_li_right icon-youce"></i>
+              </div>
             </li>
             <li class="pro_fun_li">
-              <i class="iconfont fun_li_left icon-erweima"></i>
-              <span class="fun_li_span">分享我的抖音二维码</span>
-              <i class="iconfont fun_li_right icon-youce"></i>
+              <div class="fun_li_i_left">
+                <i class="iconfont fun_li_left icon-kouling"></i>
+              </div>
+              <div class="fun_li_span">
+                <span>分享口令</span>
+              </div>
+              <div class="fun_li_i_right">
+                <i class="iconfont fun_li_right icon-youce"></i>
+              </div>
             </li>
             <li class="pro_fun_li">
-              <i class="iconfont fun_li_left icon-erweima"></i>
-              <span class="fun_li_span">分享我的抖音二维码</span>
-              <i class="iconfont fun_li_right icon-youce"></i>
+              <div class="fun_li_i_left">
+                <i class="iconfont fun_li_left icon-mianduimianjianqun"></i>
+              </div>
+              <div class="fun_li_span">
+                <span>面对面朋友</span>
+              </div>
+              <div class="fun_li_i_right">
+                <i class="iconfont fun_li_right icon-youce"></i>
+              </div>
             </li>
           </ul>
         </div>
@@ -112,8 +138,8 @@ export default {};
     // 右侧搜索内容
     .fri_sear_right {
       width: 315px;
-      border: 1px solid rgb(78, 76, 76);
-      background: rgb(78, 76, 76);
+      border: 1px solid #3f3f3f;
+      background: #3f3f3f;
       margin: 8px;
       border-radius: 3px;
       // 搜索图标
@@ -163,14 +189,20 @@ export default {};
     }
     // 功能内容
     .fri_center_pro {
-      height: 400px;
+      height: 370px;
+      margin: 7px;
+      background: #222222;
+      border-radius: 5px;
+      // 通讯录
       .pro_tongxunlu {
         height: 60px;
         display: flex;
-        justify-content: space-between;
+        justify-content: flex-start;
         align-items: center;
+        padding: 10px 0 0 0;
+        // 通讯录图标
         .pro_tongxunlu_icon {
-          background: gray;
+          background: #2f2f2f;
           border-radius: 50%;
           width: 45px;
           height: 45px;
@@ -182,36 +214,78 @@ export default {};
             font-size: 25px;
           }
         }
+        // 通讯录文字
         .pro_tongxunlu_span {
           display: flex;
           flex-direction: column;
           .pro_tongxunlu_span_top {
             font-size: 15px;
+            padding: 5px 0;
           }
           .pro_tongxunlu_span_bottom {
             font-size: 13px;
             color: gray;
           }
         }
+        // 通讯录按钮
         .pro_tongxunlu_btn {
-          width: 80px;
+          width: 180px;
           height: 30px;
-          background: red;
           display: flex;
-          align-items: center;
-          justify-content: center;
-          margin: 0 10px;
+          justify-content: flex-end;
+          .tongxunlu_btn {
+            width: 80px;
+            height: 30px;
+            background: red;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            margin: 0 10px;
+            border-radius: 2px;
+          }
         }
       }
       // 功能：分享二维码、扫一扫、分享口令、面对面加朋友
       .pro_fun {
         .pro_fun_ul {
+          list-style: none;
+          padding: 0;
           .pro_fun_li {
-            .iconfont {
+            height: 60px;
+            display: flex;
+            justify-content: flex-start;
+            align-items: center;
+            margin: 10px 0;
+            // 左侧图标
+            .fun_li_i_left {
+              background: #2f2f2f;
+              border-radius: 50%;
+              width: 45px;
+              height: 45px;
+              display: flex;
+              justify-content: center;
+              align-items: center;
+              margin: 0px 15px;
+              .iconfont {
+                font-size: 25px;
+              }
             }
-            .fun_li_left {
+            // 文字
+            .fun_li_span {
+              width: 144px;
+              font-size: 16px;
             }
-            .fun_li_right {
+            // 右侧图标
+            .fun_li_i_right {
+              width: 130px;
+              display: flex;
+              justify-content: flex-end;
+              .fun_li_right {
+                color: #686868;
+              }
+              .iconfont {
+                font-size: 16px;
+              }
             }
           }
         }
